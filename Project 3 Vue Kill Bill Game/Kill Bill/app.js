f2480c659f987891b4344c56000cb8aca27c27c4
Suttitham Jampatong <suttitham.jpt@hotmail.com>
2020-11-03 16:52:10 +0700

    Almost Done Still has Battle Log Not Working

--- Project 3 Vue Kill Bill Game/Kill Bill/app.js
+++ Project 3 Vue Kill Bill Game/Kill Bill/app.js
@@ -4,38 +4,118 @@ function getRandomValue(min, max) {
 
 
 
-
-
 const app = Vue.createApp({
-
-
-
   data() {
     return {
-      playerHP: 100,
+      playerHp: 100,
       enemyHp: 100,
+      currentRound: 0,
+      winner: null,
+      logMessages: []
     };
   },
 
 
+  computed: {
+    enemyBarStyles() {
+      if (this.enemyHp < 0) {
+        return { width: '0%' };
+      }
+      return { width: this.enemyHp + '%' }
+    },
+
+    playerBarStyles() {
+      if (this.playerHp < 0) {
+        return { width: '0%' };
+      }
+      return { width: this.playerHp + '%' }
+    },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 == 0;
+    },
+
+
+
+  },
+  watch: {
+    playerHp(value) {
+      if (value <= 0 && this.enemyHp <= 0) {
+        // a draw
+        this.winner = 'draw';
+      } else if (value <= 0) {
+        //player Lost
+        this.winner = 'enemy';
+      }
+
+    },
+    enemyHp(value) {
+      if (value <= 0 && this.playerHp <= 0) {
+        //a draw
+        this.winner = 'draw';
+      } else if (value <= 0) {
+        //Monster Lost
+        this.winner = 'player';
+      }
+    }
+  },
+
 
   methods: {
+    startGame() {
+      this.playerHp = 100;
+      this.enemyHp = 100;
+      this.winner = null;
+      this.currentRound = 0;
+      this.logMessages = [];
+
+    },
     attackEnemy() {
+      this.currentRound++;
       const attackValue = getRandomValue(5, 12);
       this.enemyHp = this.enemyHp - attackValue;
+      this.addLogMessages('player', 'attack', 'attackValue');
       this.attackPlayer();
     },
 
     attackPlayer() {
-      const attackValue = getRandomValue(8, 16);
+      const attackValue = getRandomValue(8, 15);
       this.playerHp = this.playerHp - attackValue;
-      this.attackEnemy();
-    }
+      this.addLogMessages('enemy', 'attack', 'attackValue')
+    },
+    specialAttackEnemy() {
+      this.currentRound++;
+      const attackValue = getRandomValue(10, 25);
+      this.enemyHp = this.enemyHp - attackValue;
+      this.addLogMessages('player', 'special-attack', 'attackValue')
+      this.attackPlayer();
+    },
+    healPlayer() {
+      this.currentRound++;
+      const healValue = getRandomValue(8, 20);
+      if (this.playerHp + healValue > 100) {
+        this.playerHp = 100;
+      } else {
+        this.playerHp = this.playerHp + healValue;
+      }
+      this.addLogMessages('player', 'heal', 'healValue')
+      this.attackPlayer();
+    },
+    surrender() {
+      this.winner = 'enemy';
+    },
+    addLogMessage(who, what, value) {
+      this.logMessages.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value
+      });
+    },
+
   }
 
 
 
 
-})
+});
 
 app.mount('#game')
