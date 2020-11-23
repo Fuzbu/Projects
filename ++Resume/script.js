77dc462e70c76180c5f98f9123599affafcce1ae
Suttitham Jampatong <suttitham.jpt@hotmail.com>
2020-11-23 20:28:23 +0700

    update

--- ++Resume/script.js
+++ ++Resume/script.js
@@ -1,13 +1,13 @@
 /*Menu Show*/
 const showMenu = (toggleId, navId) => {
- const toggle = document.getElementById(toggleId),
-  nav = document.getElementById(navId)
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
 
- if (toggle && nav) {
-  toggle.addEventListener('click', () => {
-   nav.classList.toggle('show')
-  })
- }
+  if (toggle && nav) {
+    toggle.addEventListener('click', () => {
+      nav.classList.toggle('show')
+    })
+  }
 }
 
 showMenu('nav-toggle', 'nav-menu')
@@ -16,12 +16,12 @@ showMenu('nav-toggle', 'nav-menu')
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction() {
- navLink.forEach(n => n.classList.remove('active'))
- this.classList.add('active')
+  navLink.forEach(n => n.classList.remove('active'))
+  this.classList.add('active')
 
- //remove menu mobile//
- const navMenu = document.getElementById('nav-menu')
- navMenu.classList.remove('show')
+  //remove menu mobile//
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show')
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction));
@@ -30,16 +30,16 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 
 //Scroll Reveal Animation//
 const sr = ScrollReveal({
- origin: 'top',
- distance: '80px',
- duration: 2000,
- reset: true
+  origin: 'top',
+  distance: '80px',
+  duration: 2000,
+  reset: true
 })
 
 //Scroll Home//
 sr.reveal('.home__title', {})
 sr.reveal('.button', { delay: 200 })
-sr.reveal('.home__img', { delay: 400 })
+// sr.reveal('.home__img', {})
 sr.reveal('.home__social-icon', { interval: 200 })
 
 
