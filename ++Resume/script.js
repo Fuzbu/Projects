d24e9dd33f1cc0027566cbc2f0b0332f70ec9b06
Suttitham Jampatong <suttitham.jpt@hotmail.com>
2020-11-24 17:16:52 +0700

    fix

--- ++Resume/script.js
+++ ++Resume/script.js
@@ -44,19 +44,23 @@ sr.reveal('.home__social-icon', { interval: 200 })
 
 
 /*Scroll About*/
+sr.reveal('.about', { delay: 400 })
 sr.reveal('.about__img', {})
-sr.reveal('.about__subtitle', { delay: 200 })
+sr.reveal('.about__subtitle', { delay: 400 })
 sr.reveal('.about__text', { delay: 400 })
 
 /*Scroll Skills */
 sr.reveal('.skills__subtitle', {})
-sr.reveal('.skills__text', { delay: 200 })
+sr.reveal('.skills__text', {})
 sr.reveal('.skills__data', { interval: 200 })
 sr.reveal('.skills__img', { delay: 400 })
 
 
 /*Scroll Work */
 sr.reveal('.work__img', { interval: 200 })
+sr.reveal('.work', { delay: 400 })
+
+
 
 /*Scroll Contact*/
 sr.reveal('.contact__input', { interval: 200 })
