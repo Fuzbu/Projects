871a935ad55d920260b8d4a2b711c3cf6151775a
Suttitham Jampatong <suttitham.jpt@hotmail.com>
2020-12-05 13:51:09 +0700

    Chapter 2 CLI

---
+++ ++VUE Learning/Class/NetNinja/Chapter 3 CLI/model-project/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1>{{ title }}</h1>
+  <Modal />
+</template>
+
+<script>
+// IMPORT COMPUNONENTS Step#1
+import Modal from "./components/Modal.vue";
+
+export default {
+  name: "App",
+
+  // REGISTER COMPONENTS Step#2
+  components: { Modal },
+
+  data() {
+    return {
+      title: "My first Vue App",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
---
+++ ++VUE Learning/Class/NetNinja/Chapter 3 CLI/model-project/src/components/Modal.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="backdrop">
+    <div class="modal">
+      <h1>Modal Title</h1>
+      <p>modal content</p>
+    </div>
+  </div>
+</template>
+
+<style>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+.modal h1 {
+  columns: #03cfb4;
+  border: none;
+  padding: 0;
+}
+
+.modal p {
+  font-style: normal;
+}
+</style>
