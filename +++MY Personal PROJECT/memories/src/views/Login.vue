ac1f6be91283a49e1682a898279cf3e89547e18a
Suttitham Jampatong <suttitham.jpt@hotmail.com>
2020-12-09 20:18:13 +0700

    Fix the Login
    Login Error

--- +++MY Personal PROJECT/memories/src/views/Login.vue
+++ +++MY Personal PROJECT/memories/src/views/Login.vue
@@ -9,19 +9,13 @@
           </p>
 
           <ul class="error-messages">
-            <li>That email is already taken</li>
+            <li v-for:="error in errors">{{ error.message }}</li>
           </ul>
 
           <form>
-            <!-- <fieldset class="form-group">
-              <input
-                class="form-control form-control-lg"
-                type="text"
-                placeholder="Your Name"
-              />
-            </fieldset> -->
             <fieldset class="form-group">
               <input
+                v-model="email"
                 class="form-control form-control-lg"
                 type="text"
                 placeholder="Email"
@@ -29,12 +23,13 @@
             </fieldset>
             <fieldset class="form-group">
               <input
+                v-model="password"
                 class="form-control form-control-lg"
-                type="password"
+                type="text"
                 placeholder="Password"
               />
             </fieldset>
-            <button class="btn btn-lg btn-primary pull-xs-right">
+            <button @click="login" class="btn btn-lg btn-primary pull-xs-right">
               Sign up
             </button>
           </form>
@@ -43,3 +38,30 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      password: "",
+      email: "",
+      errors: [],
+    };
+  },
+  methods: {
+    login() {
+      this.$store
+        .dispatch("users/loginUser", {
+          email: this.email,
+          password: this.password,
+        })
+        .then(() => {
+          this.errors = [];
+        })
+        .catch((err) => {
+          this.errors.push(err);
+        });
+    },
+  },
+};
+</script>
