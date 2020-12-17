48c0b6d5c79b965f7ddbd3df387008c2cc4b7531
Suttitham Jampatong <suttitham.jpt@hotmail.com>
2020-12-17 17:14:59 +0700

    update

--- +++MY Personal PROJECT/TailwindVue_musicplayer/postcss.config.js
+++
@@ -1,7 +0,0 @@
-// postcss.config.js
-const autoprefixer = require("autoprefixer")();
-const tailwindcss = require("tailwindcss")("./tailwind.config.js");
-
-module.exports = {
-    plugins: [tailwindcss, autoprefixer]
-};
